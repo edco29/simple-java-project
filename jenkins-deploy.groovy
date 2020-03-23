5d9c3c09102b944935db3ea00f817679e594f398
edco29 <40572443+edco29@users.noreply.github.com>
2020-03-23 09:28:46 -0500

    Create jenkins-deploy.groovy

---
+++ jenkins-deploy.groovy
@@ -0,0 +1,21 @@
+@Library('jenkins-sharedlib@master')
+import sharedlibs.jenkinsfileUtil
+
+def utils = new jenkinsfileUtil(steps, this)
+
+
+  node {
+    stage('Preparation') {
+
+      step([$class: 'WsCleanup'])
+      checkout scm
+    }
+    
+    stage('Build & U.Test') { utils.buildMaven()
+ }
+
+     // stage('Deploy') { utils.deployMaven()
+// }
+
+    
+}
